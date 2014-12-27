cd55424453b5ab428f59ff2dd9a655e3b7cdbf67
it12052 <it12052@lehre.dhbw-stuttgart.de>
2014-12-28 00:06:38 +0100

    Added AR.Drone-Libraries to Project

--- MyoPilot/MainForm.Designer.cs
+++ MyoPilot/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MyoPilot
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
--- MyoPilot/MainForm.cs
+++ MyoPilot/MainForm.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AR.Drone.Client;
+using AR.Drone.Client.Command;
+using AR.Drone.Client.Configuration;
+using AR.Drone.Data;
+using AR.Drone.Data.Navigation;
+using AR.Drone.Data.Navigation.Native;
+using AR.Drone.Media;
+using AR.Drone.Video;
 
 namespace MyoPilot
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
+            
         }
     }
 }

171dab0a5b63fdaf5f40365e6b6ac3a123cf7efc
it12052 <it12052@lehre.dhbw-stuttgart.de>
2014-12-28 20:32:16 +0100

    Streaming Video

--- MyoPilot/MainForm.Designer.cs
+++ MyoPilot/MainForm.Designer.cs
@@ -29,11 +29,41 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.timerVideoUpdate = new System.Windows.Forms.Timer(this.components);
+            this.videoFrame = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.videoFrame)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerVideoUpdate
+            // 
+            this.timerVideoUpdate.Interval = 20;
+            this.timerVideoUpdate.Tick += new System.EventHandler(this.timerVideoUpdate_Tick);
+            // 
+            // videoFrame
+            // 
+            this.videoFrame.Location = new System.Drawing.Point(25, 39);
+            this.videoFrame.Name = "videoFrame";
+            this.videoFrame.Size = new System.Drawing.Size(216, 152);
+            this.videoFrame.TabIndex = 0;
+            this.videoFrame.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.videoFrame);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.videoFrame)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timerVideoUpdate;
+        private System.Windows.Forms.PictureBox videoFrame;
     }
 }
 
--- MyoPilot/MainForm.cs
+++ MyoPilot/MainForm.cs
@@ -15,15 +15,77 @@ using AR.Drone.Data.Navigation;
 using AR.Drone.Data.Navigation.Native;
 using AR.Drone.Media;
 using AR.Drone.Video;
+using AR.Drone.WinApp;
 
 namespace MyoPilot
 {
     public partial class MainForm : Form
     {
+        private readonly DroneClient droneClient;
+        private readonly VideoPacketDecoderWorker videoPacketDecoderWorker;
+        private Settings settings;
+        private VideoFrame frame;
+        private Bitmap frameBitmap;
+        private uint frameNumber;
+        private NavigationData navigationData;
+
         public MainForm()
         {
             InitializeComponent();
-            
+
+            videoPacketDecoderWorker = new VideoPacketDecoderWorker(PixelFormat.BGR24, true, OnVideoPacketDecoded);
+            videoPacketDecoderWorker.Start();
+            videoPacketDecoderWorker.UnhandledException += UnhandledException;
+
+            droneClient = new DroneClient();
+            droneClient.VideoPacketAcquired += OnVideoPacketAcquired;
+            droneClient.NavigationDataAcquired += data => navigationData = data;
+            droneClient.Start();
+        }
+
+        private void OnVideoPacketAcquired(VideoPacket packet)
+        {
+            if (videoPacketDecoderWorker.IsAlive)
+                videoPacketDecoderWorker.EnqueuePacket(packet);
+        }
+
+        private void OnVideoPacketDecoded(VideoFrame frame)
+        {
+            this.frame = frame;
+        }
+
+        private void UnhandledException(object sender, Exception exception)
+        {
+            MessageBox.Show(exception.ToString(), "Unhandled Exception (Ctrl+C)", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Text += Environment.Is64BitProcess ? " [64-bit]" : " [32-bit]";
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            droneClient.Stop();
+            droneClient.Dispose();
+            videoPacketDecoderWorker.Dispose();
+
+            base.OnClosed(e);
+        }
+
+        private void timerVideoUpdate_Tick(object sender, EventArgs e)
+        {
+            if (frame == null || frameNumber == frame.Number)
+                return;
+            frameNumber = frame.Number;
+
+            if (frameBitmap == null)
+                frameBitmap = VideoHelper.CreateBitmap(ref frame);
+            else
+                VideoHelper.UpdateBitmap(ref frameBitmap, ref frame);
+
+            videoFrame.Image = frameBitmap;
         }
     }
 }

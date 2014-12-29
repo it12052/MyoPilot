dd76c08507befd2672f113e94a60a8b01d39274e
it12052 <it12052@lehre.dhbw-stuttgart.de>
2014-12-29 22:00:28 +0100

    Start implementing Input

---
+++ MyoPilot/Input/Input.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AR.Drone.Client.Command;
+
+namespace MyoPilot.Input
+{
+    public delegate void NoArgs();
+    public delegate void FlightCommand(FlightMode mode, float roll = 0, float pitch = 0, float yaw = 0, float gaz = 0);
+
+    public abstract class Input
+    {
+        // Events correspond to available actions in AR.Drone.Client.DroneClient
+        event NoArgs Emergency;
+        //event NoArgs ResetEmergency; // This can be done in the MainForm
+        event NoArgs Land;
+        event NoArgs Takeoff;
+        event NoArgs FlatTrim;
+        event NoArgs Hover;
+        event FlightCommand Progress;
+
+        /// <summary>
+        /// Implementors of this Class should process the state of the
+        /// peripheral (e.g. keyboard, mouse) and raise events to trigger 
+        /// actions. This method schould be invoked periodically via a Timer
+        /// </summary>
+        abstract void processInput();
+
+        void OnEmergency()
+        {
+            if (Emergency != null)
+                Emergency();
+        }
+
+        void OnLand()
+        {
+            if (Land != null)
+                Land();
+        }
+
+        void OnTakeoff()
+        {
+            if (Takeoff != null)
+                Takeoff();
+        }
+
+        void OnFlatTrim()
+        {
+            if (FlatTrim != null)
+                FlatTrim();
+        }
+
+        void OnHover()
+        {
+            if (Hover != null)
+                Hover();
+        }
+
+        void OnProgress(FlightMode mode, float roll = 0, float pitch = 0, float yaw = 0, float gaz = 0)
+        {
+            if (Emergency != null)
+                Progress(mode, roll, pitch, yaw, gaz);
+        }
+    }
+}
---
+++ MyoPilot/Input/InputManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyoPilot.Input
+{
+    /// <summary>
+    /// Container 
+    /// </summary>
+    class InputManager : Input
+    {
+        public event NoArgs Emergency;
+        public event NoArgs Land;
+        public event NoArgs Takeoff;
+        public event NoArgs FlatTrim;
+        public event NoArgs Hover;
+        public event FlightCommand Progress;
+
+        public void processInput()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

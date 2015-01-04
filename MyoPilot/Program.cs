f5f001d46035e96a80b3019614539ee5ee20a129
it12052 <it12052@lehre.dhbw-stuttgart.de>
2015-01-04 21:42:18 +0100

    Fixed missing FFMPEG libs

--- MyoPilot/Program.cs
+++ MyoPilot/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AR.Drone.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,6 +15,21 @@ namespace MyoPilot
         [STAThread]
         static void Main()
         {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                case PlatformID.Win32S:
+                case PlatformID.Win32Windows:
+                    string ffmpegPath = string.Format(@"../../../AR.Drone/FFmpeg.AutoGen/FFmpeg/bin/windows/{0}", Environment.Is64BitProcess ? "x64" : "x86");
+                    InteropHelper.RegisterLibrariesSearchPath(ffmpegPath);
+                    break;
+                case PlatformID.Unix:
+                case PlatformID.MacOSX:
+                    string libraryPath = Environment.GetEnvironmentVariable(InteropHelper.LD_LIBRARY_PATH);
+                    InteropHelper.RegisterLibrariesSearchPath(libraryPath);
+                    break;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
